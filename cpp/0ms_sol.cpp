ba28bf334177c7212cc3e37cde37f053cb7df35c
MrEuroBlue <MrEuroBlue@MREUROPC>
2021-08-06 16:04:51 +0200

    some changes

--- cpp/0ms_sol.cpp
+++ cpp/0ms_sol.cpp
@@ -1,20 +1,17 @@
 #include <unordered_map>
-class Solution {
-    std::unordered_map<int, int> dict
-    {
-        {0,0},{1,1}
-    };
-public:
-    int fib(int n) {
-        if(dict.find(n) != dict.end())
-        {
-            return dict[n];
-        }
-        else
-        {
-            std::pair<int,int> x={n,(fib(n-1)+fib(n-2))};
-            dict.insert(x);
-            return dict[n];
-        }
-    }
+std::unordered_map<int, int> dict
+{
+    {0,0},{1,1}
 };
+int fib(int n) {
+    if(dict.find(n) != dict.end())
+    {
+        return dict[n];
+    }
+    else
+    {
+        std::pair<int,int> x={n,(fib(n-1)+fib(n-2))};
+        dict.insert(x);
+        return dict[n];
+    }
+}
