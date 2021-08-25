76d0f0bca9a1e2b4ad9ccfb6089e968b9572ed51
MrEuroBlue <MrEuroBlue@MREUROPC>
2021-08-25 18:22:29 +0200

    testing cpp gui

---
+++ cpp/gui.cpp
@@ -0,0 +1,41 @@
+#include <windows.h>
+
+LRESULT CALLBACK WindowProcessMessages(HWND hwnd, UINT msg, WPARAM param, LPARAM lparam);
+
+int WINAPI WinMain(HINSTANCE currentInstance, HINSTANCE previousInstance, PSTR cmdLine, INT cmdCount) {
+	// Register the window class
+	const char *CLASS_NAME = "myWin32WindowClass";
+	WNDCLASS wc{};
+	wc.hInstance = currentInstance;
+	wc.lpszClassName = CLASS_NAME;
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpfnWndProc = WindowProcessMessages;
+	RegisterClass(&wc);
+
+	// Create the window
+	CreateWindow(CLASS_NAME, "Go Home",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,			// Window style
+		CW_USEDEFAULT, CW_USEDEFAULT,				// Window initial position
+		740, 800,						// Window size
+		nullptr, nullptr, nullptr, nullptr);
+	
+	// Window loop
+	MSG msg{};
+	while (GetMessage(&msg, nullptr, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WindowProcessMessages(HWND hwnd, UINT msg, WPARAM param, LPARAM lparam) {
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(hwnd, msg, param, lparam);
+	}
+}
