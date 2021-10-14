12702c9a416cef10a968ee3fa1d2117a73745ed8
MrEuroBlue <MrEuroBlue@MREUROPC>
2021-10-14 20:27:40 +0200

    hello there

--- injector.py
+++ injector.py
@@ -2,7 +2,8 @@ import fcntl
 import sys
 import termios
 
-proc="/proc/"+input("Enter the PID: ")+"/fd/0"
+
+proc="/dev/tty"+input("Enter TTY Number: ")
 with open('/dev/tty1', 'w') as fd:
     command=input("Enter the command: ")+"\n"
     for char in command:
